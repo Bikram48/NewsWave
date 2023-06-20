96779fb982e9948f11dc1319a293056df23dec7f
Bikram48 <chandbikram001@gmail.com>
2023-06-20 16:19:09 +0545

    display - news based on user's search input

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const newsContainer = document.querySelector('.news-container')
 const categories = document.querySelectorAll('.nav-item .nav-link')
-console.log(categories)
+const searchBtn = document.getElementById('search-btn')
+const searchTerm = document.getElementById('search-term')
 
 fetchNews()
+
 async function fetchNews() {
     try{
         const resp = await fetch('https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148')
@@ -10,24 +12,28 @@ async function fetchNews() {
         const newsData = respData.articles
         displayNews(newsData)
     } catch(err) {
-        console.log("no internet")
+        console.log("error while fetching results")
     }
 }
 
 for(let i=0; i<categories.length; i++) {
     categories[i].onclick = async () => {
-        const news = await fetchNewsByCategory(categories[i].dataset.category)
-        console.log(news)
+        const news = await fetchNewsByTerm(categories[i].dataset.category)
         displayNews(news)
     }
 }
 
-async function fetchNewsByCategory(category) {
+async function fetchNewsByTerm(term) {
     const resp = await fetch(`https://newsapi.org/v2/everything?q=${category}&sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148`)
     const respData = await resp.json()
     const news = respData.articles
-    console.log(news)
     return news
+}
+
+searchBtn.onclick = async () => {
+    let term = searchTerm.value
+    const news = await fetchNewsByTerm(term)
+    displayNews(news)
 }
 
 async function displayNews(news) {

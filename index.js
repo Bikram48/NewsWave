b8ba727323c079deb1afa7193269523d22c4f88a
Bikram48 <chandbikram001@gmail.com>
2023-06-20 15:49:33 +0545

    display - all bbc news

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-fetchNews()
+const newsContainer = document.querySelector('.news-container')
+
+displayNews()
 async function fetchNews() {
-    const resp = await fetch('https://newsapi.org/v2/everything?q=apple&from=2023-06-19&to=2023-06-19&sortBy=popularity&apiKey=5235a3e37c4f470ca8af56361d4f8148')
-    const respData = await resp.json()
-    const newsData = respData.articles
+    try{
+        const resp = await fetch('https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148')
+        const respData = await resp.json()
+        const newsData = respData.articles
+        return newsData
+    } catch(err) {
+        console.log("no internet")
+    }
 }
 
+async function displayNews() {
+    const news = await fetchNews()
+    news.forEach(news => allNews(news))
+}
+
+function allNews(news) {
+    const el = document.createElement('div')
+    el.classList.add('col-md-4')
+    el.innerHTML = `
+    <div class="card">
+        <img class="card-img-top" src=${news.urlToImage} alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${news.title}</h5>
+          <p class="card-text">${news.content}</p>
+          <a href=${news.url} target="_blank"  class="btn btn-primary">Read More</a>
+        </div>
+    </div>
+    `
+    newsContainer.appendChild(el)
+}

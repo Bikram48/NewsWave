c69e483fb269f9aa7a04c8d70f7651ae573ed400
Bikram48 <chandbikram001@gmail.com>
2023-06-21 16:14:22 +0545

    implement - load more button

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const categories = document.querySelectorAll('.nav-item .nav-link')
 const searchBtn = document.getElementById('search-btn')
 const searchTerm = document.getElementById('search-term')
 const spinner = document.getElementById('spinner')
+const loadMoreBtn = document.getElementById('loadmore-btn')
+let page = 1
+let data = []
+let isCategory = false
+let category = ''
 
 fetchNews()
 
-async function fetchNews() {
+loadMoreBtn.onclick = async() => {
+    page += 1
+    if(!isCategory) {
+        fetchNews(page)
+    } else {
+        const data = await fetchNewsByTerm(category)
+        displayNews(data)
+    }
+}
+
+async function fetchNews(page=1) {
     try{
-        const resp = await fetch('https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148')
-        console.log(resp)
+        const resp = await fetch(`https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148&page=${page}&pageSize=9`)
         const respData = await resp.json()
         const newsData = respData.articles
-        displayNews(newsData)
+        data.push(...newsData)
+        displayNews(data)
+ 
     } catch(err) {
         console.log("error while fetching results")
     }
@@ -20,18 +36,23 @@ async function fetchNews() {
 
 for(let i=0; i<categories.length; i++) {
     categories[i].onclick = async () => {
-        const news = await fetchNewsByTerm(categories[i].dataset.category)
+        data = []
+        page = 1
+        category = categories[i].dataset.category
+        const news = await fetchNewsByTerm(category)
         displayNews(news)
     }
 }
 
 async function fetchNewsByTerm(term) {
+    isCategory = true
     displayLoading(true)
-    const resp = await fetch(`https://newsapi.org/v2/everything?q=${term}&sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148`)
+    const resp = await fetch(`https://newsapi.org/v2/everything?q=${term}&sources=bbc-news&apiKey=5235a3e37c4f470ca8af56361d4f8148&page=${page}&pageSize=9`)
     const respData = await resp.json()
     displayLoading(false)
     const news = respData.articles
-    return news
+    data.push(...news)
+    return data
 }
 
 searchBtn.onclick = async () => {
@@ -64,10 +85,8 @@ function allNews(news) {
 
 function displayLoading(isLoading) {
     if(isLoading) {
-        spinner.src = 'loading.gif'
         spinner.style.display = 'block'
     } else {
-        spinner.src=''
         spinner.style.display = 'none'
     }
 }
